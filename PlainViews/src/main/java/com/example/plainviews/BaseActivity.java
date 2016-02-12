51a7666b3a89f9017a0562b06edfc4dea6490923
Wojciech Plesiak <wojciech.plesiak@mobica.com>
2016-02-12 21:42:58 +0100

    Revert "initial commit containing almost blank, working Plain Views application"
    This reverts commit 794f8fe14f209ea60f32c9620b009dc37130987a.

--- PlainViews/src/main/java/com/example/plainviews/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.plainviews;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by piha on 2016-02-01.
- */
-public class BaseActivity extends AppCompatActivity {
-}
--- PlainViews/src/main/java/com/example/plainviews/widget/RtlViewPager.java
+++
@@ -1,7 +0,0 @@
-package com.example.plainviews.widget;
-
-/**
- * Created by piha on 2016-02-02.
- */
-public class RtlViewPager {
-}
