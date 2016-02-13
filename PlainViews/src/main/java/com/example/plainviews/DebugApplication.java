c3d82ab92c38bc4bb00a496c1f8cb1dbf2b74cf0
Wojciech Plesiak <wojciech.plesiak@mobica.com>
2016-02-13 01:23:24 +0100

    Copying all resources, main Activity and main Fragments. Commenting out dependencies. Dirty code.

---
+++ PlainViews/src/main/java/com/example/plainviews/DebugApplication.java
@@ -0,0 +1,14 @@
+package com.example.plainviews;
+
+import android.app.Application;
+
+/**
+ * Application class
+ */
+public class DebugApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
